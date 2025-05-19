c0011443d7d4763b9ec5538b30e25fcb0e591d73
xix0one <petrovalex2014@ya.ru>
2025-05-19 15:51:25 +0800

    added change sizes and added button create div

--- constructor.js
+++ constructor.js
@@ -1,11 +1,12 @@
 "use strict";
 
+let right_panel = document.getElementById("right");
+let left_panel = document.getElementById("left");
 
 // download from localStorage 
 
 (function load_body() {
     const elems = localStorage.getItem('data');
-    let right_panel = document.getElementById("right");
     if (elems) {
         right_panel.innerHTML = elems;
         right_panel.style.width = localStorage.getItem("width");
@@ -22,9 +23,44 @@
 // help text ----------------
 
 function print_HTML_code() {
-    let right_panel = document.getElementById("right");
     let code = document.getElementById("code");
-    code.textContent = right_panel.innerHTML;
+    code.textContent = right_panel.innerHTML.replace(/></g, ">\n<");
+}
+
+// --------------------------
+
+
+// styles added elems -------
+
+function start_button_style_menu(event) {
+    
+    event.target.style.borderColor = "red";
+    print_HTML_code();
+}
+
+function start_div_style_menu(event) {
+    print_HTML_code();
+}
+
+// --------------------------
+
+
+// add elems ----------------
+
+function add_button() {
+    let btn = document.createElement("button");
+    btn.textContent = "button";
+    btn.addEventListener("click", start_button_style_menu);
+    right_panel.append(btn);
+}
+
+function add_div() {
+    let div = document.createElement("div");
+    div.style.border = "1px solid black";
+    div.style.height = "120px";
+    div.style.width = "130px";
+    div.addEventListener("click", start_div_style_menu);
+    right_panel.append(div);
 }
 
 // --------------------------
@@ -32,22 +68,15 @@ function print_HTML_code() {
 
 // add controls -------------
 
-function add_button() {
-    let btn = document.createElement("button");
-    btn.textContent = "button";
-    right.append(btn);
-    print_HTML_code();
-}
-
 function clear() {
     localStorage.removeItem('data');
     document.location.reload();
 }
 
 function save() {
-    const right_panel = document.getElementById("right");
     const right_panel_height = right_panel.style.height;
     const right_panel_width = right_panel.style.width;
+
     localStorage.setItem("data", right_panel.innerHTML);
     localStorage.setItem("height", right_panel_height);
     localStorage.setItem("width", right_panel_width);
@@ -58,11 +87,14 @@ function add_event(el, control) {
         if (control === "addbutton") add_button();
         else if (control === "clear") clear();
         else if (control === "save") save();
+        else if (control === "add_div") add_div();
+
+        print_HTML_code();
     });
 }
 
-function add_element_control(type, text, id, control_type) {
-    let element = document.createElement(type);
+function add_button_control(text, id, control_type) {
+    let element = document.createElement("button");
     element.textContent = text;
     element.id = id;
     add_event(element, control_type);
@@ -70,11 +102,10 @@ function add_element_control(type, text, id, control_type) {
 }
 
 function add_main_controls() {
-    let left = document.getElementById("left");
-
-    left.append(add_element_control("button", "add button", "button_addbuttons", "addbutton"));
-    left.append(add_element_control("button", "clear", "clear", "clear"));
-    left.append(add_element_control("button", "save", "save", "save"));
+    left_panel.append(add_button_control("add button", "button_addbuttons", "addbutton"));
+    left_panel.append(add_button_control("add div", "div", "add_div"));
+    left_panel.append(add_button_control("clear", "clear", "clear"));
+    left_panel.append(add_button_control("save", "save", "save"));
 }
 
 add_main_controls();
