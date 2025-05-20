917ef3c027eee6f882e84d47861d921e2870f2e1
xix0one <petrovalex2014@ya.ru>
2025-05-20 11:43:16 +0800

    global changes

--- constructor.js
+++ constructor.js
@@ -3,111 +3,139 @@
 let right_panel = document.getElementById("right");
 let left_panel = document.getElementById("left");
 
-// download from localStorage 
-
-(function load_body() {
-    const elems = localStorage.getItem('data');
-    if (elems) {
-        right_panel.innerHTML = elems;
-        right_panel.style.width = localStorage.getItem("width");
-        right_panel.style.height = localStorage.getItem("height");
-        print_HTML_code();
-    } else {
-        save();
-    }
-})();
-
-// --------------------------
-
-
-// help text ----------------
-
 function print_HTML_code() {
     let code = document.getElementById("code");
     code.textContent = right_panel.innerHTML.replace(/></g, ">\n<");
 }
 
-// --------------------------
+function remove_panel(idf, main_text) {
+    let remove_current_panel = document.getElementById("setting_panel " + idf);
+    let select = document.getElementById(idf + " select");
+    select.value = main_text;
+    remove_current_panel.remove();
+}
 
-
-// styles added elems -------
-
-function start_button_style_menu(event) {
+// create text or block element
+// function add_element(event, idf, input, select_value) {
+//     if (idf === "text" && !input.value) {
+//         input.style.borderColor = "red";
+//     } else {
+//         let el = document.createElement(select_value);
     
-    event.target.style.borderColor = "red";
-    print_HTML_code();
+//         if (input.value) {
+//             el.textContent = input.value;
+//         }
+
+//         if (idf === "form") {
+//             el.style.border = "1px solid black";
+//             el.style.height = "40px";
+//             el.style.width = "70px";
+
+//             el.addEventListener("click", function() {
+//                 print_HTML_code();
+//             });
+//         }
+        
+//         if (idf === "text") {
+//             let color = document.getElementById("input_color");
+//             el.style.color = color.value;
+//         }
+
+//         right_panel.append(el);
+//         print_HTML_code();
+//     }
+// }
+
+function default_form_setting(idf, div) {
+    div.id = "setting_panel " + idf;
+
+    let input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "type text";
+    div.append(input);
 }
 
-function start_div_style_menu(event) {
-    print_HTML_code();
+// function deafult_button_setting(event, idf, input, select_value, main_text) {
+//     let btn = document.createElement("button");
+//     btn.textContent = "create";
+//     btn.addEventListener("click", function() {
+//         add_element(event, idf, input, select_value);
+//     });
+
+//     div.append(btn);
+
+//     let btn_close = document.createElement("button");
+//     btn_close.textContent = "close";
+//     btn_close.addEventListener("click", function() {
+//         remove_panel(idf, main_text);
+//     });
+
+//     div.append(btn_close);
+// }
+
+function create_text_color_setting_panel() {
+    let inner_div = document.createElement("div");
+
+    let span = document.createElement("span");
+    span.textContent = "color: ";
+    inner_div.append(span);
+
+    let input_color = document.createElement("input");
+    input_color.type = "color";
+    input_color.id = "input_color";
+
+    inner_div.append(input_color);
+
+    return inner_div;
 }
 
-// --------------------------
+function create_setting_panel(event, idf, main_text) {
+    const select_value = event.target.value;
+    let setting_panel = document.getElementById(idf);
 
+    if (!document.getElementById(("setting_panel " + idf))) {
 
-// add elems ----------------
+        let div = document.createElement("div");
+        default_form_setting(idf, div);
 
-function add_button() {
-    let btn = document.createElement("button");
-    btn.textContent = "button";
-    btn.addEventListener("click", start_button_style_menu);
-    right_panel.append(btn);
+        if (idf === "text") {
+            div.append(create_text_color_setting_panel());
+        }
+
+        //deafult_button_setting(event, idf, input, select_value, main_text);
+
+        setting_panel.append(div);
+    }
 }
 
-function add_div() {
+function create_drop_down_list(main_text, options, idf) {
     let div = document.createElement("div");
-    div.style.border = "1px solid black";
-    div.style.height = "120px";
-    div.style.width = "130px";
-    div.addEventListener("click", start_div_style_menu);
-    right_panel.append(div);
-}
+    div.id = idf;
 
-// --------------------------
+    let select = document.createElement("select");
+    select.id = idf + " select";
 
+    let placeholder = new Option(main_text);
+    placeholder.disabled = true;
+    placeholder.selected = true;
+    select.add(placeholder);
 
-// add controls -------------
+    for (let i = 0; i < options.length; ++i) {
+        let option = new Option(options[i]);
+        select.add(option);
+    }
 
-function clear() {
-    localStorage.removeItem('data');
-    document.location.reload();
-}
-
-function save() {
-    const right_panel_height = right_panel.style.height;
-    const right_panel_width = right_panel.style.width;
-
-    localStorage.setItem("data", right_panel.innerHTML);
-    localStorage.setItem("height", right_panel_height);
-    localStorage.setItem("width", right_panel_width);
-}
-
-function add_event(el, control) {
-    el.addEventListener("click", function() {
-        if (control === "addbutton") add_button();
-        else if (control === "clear") clear();
-        else if (control === "save") save();
-        else if (control === "add_div") add_div();
-
-        print_HTML_code();
+    select.addEventListener('change', function(event) {
+        create_setting_panel(event, idf, main_text);
     });
+    
+    div.append(select);
+    return div;
 }
 
-function add_button_control(text, id, control_type) {
-    let element = document.createElement("button");
-    element.textContent = text;
-    element.id = id;
-    add_event(element, control_type);
-    return element;
+function create_left_menu() {
+    left_panel.append(create_drop_down_list("text elements", ["h1", "h2", "h3", "p", "span"], "text"));
+    left_panel.append(create_drop_down_list("block elements", ["div", "header", "footer", "aside", "table"], "block"));
 }
 
-function add_main_controls() {
-    left_panel.append(add_button_control("add button", "button_addbuttons", "addbutton"));
-    left_panel.append(add_button_control("add div", "div", "add_div"));
-    left_panel.append(add_button_control("clear", "clear", "clear"));
-    left_panel.append(add_button_control("save", "save", "save"));
-}
-
-add_main_controls();
-
-// --------------------------
+create_left_menu();
